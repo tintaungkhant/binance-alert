5bc0cdbbd93210b025becec37d5ca8e31046a72f
Tint Aung Khant <aherkersoemin@gmail.com>
2024-06-27 18:59:18 +0700

    chore: test

--- src/scraper.ts
+++ src/scraper.ts
@@ -158,7 +158,11 @@ export default class Scraper {
         };
 
         await axios.get(url, { params }).catch((error) => {
-            console.log(error);
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+              }
         });
     }
 
